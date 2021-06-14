0313f062c5b4cc168fb51fb4087b195e61a7a591
nguyennguyen1112000 <nguyennguyen1112000@gmail.com>
2021-06-14 09:45:56 +0700

    delete resolver

--- libs/crud-typeorm/src/crud-typeorm.module.ts
+++ libs/crud-typeorm/src/crud-typeorm.module.ts
@@ -3,7 +3,6 @@ import { CrudTypeormService } from './crud-typeorm.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BaseEntity } from './entities/base.entity'
 import { CrudTypeormController } from './crud-typeorm.controller';
-import { CrudTypeormResolver } from './crud-typeorm.resolver';
 @Module({
   imports: [TypeOrmModule.forFeature([BaseEntity])],
   providers: [CrudTypeormService],
